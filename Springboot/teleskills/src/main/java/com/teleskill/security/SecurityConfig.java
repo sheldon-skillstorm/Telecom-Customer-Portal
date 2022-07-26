c15af8b15b6049f3bfb886c1e0152b6ad00f61e1
Sheldon <sheldon.nico.lawrence@gmail.com>
2022-07-26 16:25:31 -0400

    different tries at security

--- Springboot/teleskills/src/main/java/com/teleskill/security/SecurityConfig.java
+++ Springboot/teleskills/src/main/java/com/teleskill/security/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
@@ -26,41 +29,107 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 	
+//	@Autowired
+//	private UserDetailsService userDetailsService;
+//	
+//	@Bean
+//	AuthenticationProvider authenticationProvider() {
+//		DaoAuthenticationProvider provider
+//		                 = new DaoAuthenticationProvider();
+//		provider.setUserDetailsService(userDetailsService);
+//		provider.setPasswordEncoder(new BCryptPasswordEncoder());
+//		return provider;
+//	}
+//	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception{
+//		http.authorizeRequests()
+//		.antMatchers("/")
+//		.permitAll()
+//		.antMatchers("/plans/v1")
+//		.hasAuthority("ROLE_ADMIN")
+//		.anyRequest()
+//		.authenticated()
+//		.and()
+//		.httpBasic();
+//	}
+
+	
+	
+	
+	///---------------------------------------------------------------------
+//	@Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.httpBasic();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("*");
+//    }
+	
+	//------------------------------------------------------------------------------------------
 
 	 @Autowired
 	    private DataSource dataSource;
+	 
+		@Autowired
+	    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+	        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+	            .dataSource(dataSource)
+	            .usersByUsernameQuery("select username, password, enabled from users where username=?")
+	            .authoritiesByUsernameQuery("select username, role from users where username=?")
+	        ;
+	            
+		}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http.authorizeRequests()
-		            .mvcMatchers("/plans/**")
-		            .hasRole("USER")
-		            .anyRequest()
-		            .authenticated()
-		            .and()
-		            .formLogin()
-		           .loginPage("/login")
+	 @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http.authorizeRequests()
+	            .anyRequest().authenticated()
+	            .and()
+	            .formLogin().permitAll()
+	            .and()
+	            .logout().permitAll();     
+	    }
+		
+		
+		
+//-------------------------------------------------------------------------------------------		
+		
+		
+		
+		
+		
+		
+//		            .mvcMatchers("/plans/**")
+//		            .hasRole("Role_USER")
+//		            .anyRequest()
+//		            .authenticated()
+//		            .and()
+//		            .formLogin()
+//		           .loginPage("/login")
 		           
-		           .and()
-	               .cors()
-	               .and()
-	               .csrf()
-	               .disable();
-	}
+//		           .and()
+//	               .cors()
+//	               .and()
+//	               .csrf()
+//	               .disable();
 	
-	@Autowired
-    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-            .dataSource(dataSource)
-            .usersByUsernameQuery("select username, password, enabled from users where username=?")
-            .authoritiesByUsernameQuery("select username, role from authorities where username=?")
-            ;
-	}
 	
-	@Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+
+	
+//	@Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 
 //	    @Bean
 //	    CorsConfigurationSource corsConfigurationSource() {
@@ -77,11 +146,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
 	   
 		
 		
-	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-		        .allowedOrigins("*")
-		        .allowedMethods("*");
-	}
+//	
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**")
+//		        .allowedOrigins("*")
+//		        .allowedMethods("*");
+//	}
 }
--- Springboot/teleskills/src/main/resources/schema.sql
+++ Springboot/teleskills/src/main/resources/schema.sql
@@ -1,17 +1,9 @@
 
-create table users(
-	username varchar(50) not null primary key,
-	password varchar(100) not null,
-	enabled boolean not null
-	);
-insert into users values('dan', 'pickles', true); 
-
-
-create table authorities (
-		username varchar(50) not null,
-		authority varchar(50) not null,
-		constraint fk_authorities_users foreign key(username) references users(username)
-	);
-create unique index ix_auth_username on authorities (username,authority);
-insert into authorities values('Jack', 'ROLE_USER');
-insert into authorities values('Jill', 'ROLE_ADMIN');username on authorities (username,authority);
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  `enabled` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+);
