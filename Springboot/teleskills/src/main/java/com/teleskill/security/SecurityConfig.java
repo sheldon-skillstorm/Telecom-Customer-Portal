588bb18aaa527174885191e36e95d1382ec3d1d7
Sheldon <sheldon.nico.lawrence@gmail.com>
2022-07-26 13:33:30 -0400

    2nd merge try

--- Springboot/teleskills/src/main/java/com/teleskill/security/SecurityConfig.java
+++ Springboot/teleskills/src/main/java/com/teleskill/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,35 +20,41 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 	
 
-	
-	@Autowired
-	DataSource dataSource;
-	
-	@Override
-	public void configure(AuthenticationManagerBuilder authBuilder) throws Exception {
-		authBuilder.jdbcAuthentication().dataSource(dataSource);
-		
-	}
+	 @Autowired
+	    private DataSource dataSource;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests()
-		           .anyRequest()
-		           .authenticated()
-		           .and()
-		           .formLogin()
+		            .mvcMatchers("/plans/**")
+		            .hasRole("USER")
+		            .anyRequest()
+		            .authenticated()
+		            .and()
+		            .formLogin()
 		           .loginPage("/login")
-		           .permitAll();
-//		           .and()
-//	               .cors()
-//	               .and()
-//	               .csrf()
-//	               .disable();
+		           
+		           .and()
+	               .cors()
+	               .and()
+	               .csrf()
+	               .disable();
+	}
+	
+	@Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+            .dataSource(dataSource)
+            .usersByUsernameQuery("select username, password, enabled from users where username=?")
+            .authoritiesByUsernameQuery("select username, role from authorities where username=?")
+            ;
 	}
 	
 	@Bean
@@ -55,23 +62,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-	    @Bean
-	    CorsConfigurationSource corsConfigurationSource() {
-	        CorsConfiguration configuration = new CorsConfiguration();
-	        configuration.setAllowedOrigins(Arrays.asList("*"));
-	        configuration.setAllowedMethods(Arrays.asList("*"));
-	        configuration.setAllowedHeaders(Arrays.asList("*"));
-	        configuration.setAllowCredentials(true);
-	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	        source.registerCorsConfiguration("/**", configuration);
-	        return source;
-	    }
+//	    @Bean
+//	    CorsConfigurationSource corsConfigurationSource() {
+//	        CorsConfiguration configuration = new CorsConfiguration();
+//	        configuration.setAllowedOrigins(Arrays.asList("*"));
+//	        configuration.setAllowedMethods(Arrays.asList("*"));
+//	        configuration.setAllowedHeaders(Arrays.asList("*"));
+//	        configuration.setAllowCredentials(true);
+//	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//	        source.registerCorsConfiguration("/**", configuration);
+//	        return source;
+//	    }
+//	    
+	   
+		
+		
 	
-	
-//	@Override
-//	public void addCorsMappings(CorsRegistry registry) {
-//		registry.addMapping("/**")
-//		        .allowedOrigins("*")
-//		        .allowedMethods("*");
-//	}
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		        .allowedOrigins("*")
+		        .allowedMethods("*");
+	}
 }
