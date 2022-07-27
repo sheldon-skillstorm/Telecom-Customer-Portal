8e85ac6387e9e1f8ae388a372a6c7dc86e710b6e
Sheldon <sheldon.nico.lawrence@gmail.com>
2022-07-27 19:29:53 -0400

    check

--- src/app/services/plan.service.ts
+++ src/app/services/plan.service.ts
@@ -1,15 +1,15 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Plan } from '../models/plan.model';
+import { Plan } from '../Models/plan.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlanService {
-  url: string = "http://localhost:8080/plans/v1";
+  url: string = 'http://localhost:8080/plans/v1';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   findAll(): Observable<HttpResponse<Plan[]>> {
     return this.http.get<Plan[]>(this.url, { observe: 'response' });
